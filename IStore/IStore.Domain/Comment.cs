9d8a9575f144ce904ee80a8718df6f7e75683834
Sofia <sofiakrakova84@gmail.com>
2020-09-06 11:59:20 +0300

    Add some base types

--- IStore/IStore.Domain/Comment.cs
+++ IStore/IStore.Domain/Comment.cs
@@ -5,9 +5,9 @@ namespace IStore.Domain
     public class Comment
     {
         public int Id { get; set; }
-        public int GoodId { get; set; }
+        public int ItemId { get; set; }
         public int? ParentId { get; set; }
-        
+
         public DateTime Timestamp { get; set; }
         public string Author { get; set; }
         public string Text { get; set; }
---
+++ IStore/IStore.Domain/ItemCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IStore.Domain
+{
+    public class ItemCard
+    {
+        public string ImageBase64 { get; set; }
+        public double Price { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int DeliveryTime { get; set; }
+        public List<Comment> Comments { get; set; }
+
+        public ItemCard(string imageBase64, double price, string description, string title)
+        {
+            if (price < 0)
+                throw new ArgumentException("Price cannot be less than 0", nameof(price));
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Title cannot be empty", nameof(title));
+
+            ImageBase64 = imageBase64;
+            Price = price;
+            Description = description;
+            Title = title;
+            
+            Comments = new List<Comment>();
+        }
+    }
+}
