08b8e4597d09813ebb26e6303e0599418b8ed16f
Sofia <sofiakrakova84@gmail.com>
2020-09-06 11:44:51 +0300

    Add domain and test project

---
+++ IStore/IStore.Domain/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IStore.Domain
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int GoodId { get; set; }
+        public int? ParentId { get; set; }
+        
+        public DateTime Timestamp { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+    }
+}
