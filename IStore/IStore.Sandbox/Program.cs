12a707c9fb2ed72238a30135dd1b20d43c3adbda
Sofia <sofiakrakova84@gmail.com>
2020-09-09 03:07:30 +0300

    Add db scheme

--- IStore/IStore.Sandbox/Program.cs
+++ IStore/IStore.Sandbox/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using IStore.Data;
+using IStore.Domain;
+using System;
+using System.Data.Common;
+using System.Text;
 
 namespace IStore.Sandbox
 {
@@ -7,6 +11,18 @@ namespace IStore.Sandbox
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            DbConnectionStringBuilder sb = new DbConnectionStringBuilder();
+            sb.Add("Server", "localhost");
+            sb.Add("Database", "istoredb");
+            sb.Add("User Id", "root");
+            sb.Add("Password", "admin");
+
+            CommentRepository commentRepository = new CommentRepository(sb.ConnectionString);
+            Comment comment = commentRepository.Get(1);
+            Console.WriteLine(comment.Text);
+
+            Console.ReadLine();
         }
     }
 }
