68b8238e738bb7eed952214d1b0ced8124a157d9
Sofia <sofiakrakova84@gmail.com>
2020-10-30 14:06:18 +0300

    Add validation attrubutes

--- IStore/IStore.Web/Models/RegistrationViewModel.cs
+++ IStore/IStore.Web/Models/RegistrationViewModel.cs
@@ -5,15 +5,18 @@ namespace IStore.Web.Models
     public class RegistrationViewModel
     {
         [DataType(DataType.Text)]
+        [MinLength(2)]
         public string Credentials { get; set; }
 
         [EmailAddress]
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
+        [MinLength(4)]
         public string Password { get; set; }
-        
+
         [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
         public string PasswordConfirmation { get; set; }
 
         [DataType(DataType.MultilineText)]
