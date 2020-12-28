cca7e0f4315e0271bf93d89d6ff67d7be70277ad
sofiakrakova <sofiakrakova84@gmail.com>
2020-12-29 00:16:13 +0300

    Update and Correct Error in Cart

--- IStore/IStore.Web/Controllers/CartController.cs
+++ IStore/IStore.Web/Controllers/CartController.cs
@@ -12,20 +12,59 @@ namespace IStore.Web.Controllers
     public class CartController : Controller
     {
         IProductsRepository _productsRepository;
+        IOrdersRepository _ordersRepository;
         ILogger<CartController> _logger;
-        public CartController(IProductsRepository productsRepository, ILogger<CartController> logger)
+        public CartController(IProductsRepository productsRepository, ILogger<CartController> logger, IOrdersRepository ordersRepository)
         {
             _logger = logger;
             _productsRepository = productsRepository;
+            _ordersRepository = ordersRepository;
+        }
+
+        public IActionResult AddOrder(string country, string area, string city, string street, string house, string kv, string paymentType, DateTime date)
+        {
+            string address = $"{country}, {area}, г. {city}, ул. {street}, д. {house}, кв. {kv}";
+            Order order = new Order
+            {
+                OrderItems = CreateOrderList(),
+                OrderDate = DateTime.Now,
+                DeliveryDate = date, 
+                Address = address,
+                Status = "Created",
+                PaymentType = paymentType,
+                Id = _ordersRepository.GetAll().Count(),
+                User_Id = 1,
+            };
+            _ordersRepository.Create(order);
+            return Redirect($"~/Cart");
         }
 
         public IActionResult DeleteItem(int id)
         {
-            Response.Cookies.Delete($"product{id}");
+            DeleteIdCookie(id);
             return Redirect($"~/Cart");
         }
 
-        public ViewResult Index()
+        private IEnumerable<OrderItem> CreateOrderList()
+        {
+            Dictionary<int, int> products = GetProducts();
+            List<OrderItem> items = new List<OrderItem>();
+
+            foreach (var item in products)
+            {
+                items.Add(new OrderItem
+                {
+                    Product = _productsRepository.Get(item.Key),
+                    Qty = item.Value,
+                });
+                DeleteIdCookie(item.Key);
+            }
+            return items;
+        }
+
+        private void DeleteIdCookie(int id) => Response.Cookies.Delete($"product{id}");
+
+        private Dictionary<int, int> GetProducts()
         {
             //словарь товар-его количество
             Dictionary<int, int> products = new Dictionary<int, int>();
@@ -46,6 +85,14 @@ namespace IStore.Web.Controllers
             }
 
             ViewData["length"] = length;
+            return products;
+        }
+
+        public ViewResult Index()
+        {
+            
+            Dictionary<int, int> products = GetProducts();
+
             ViewData["products"] = _productsRepository;
 
             return View(products);
